cc2d22fa5c7b473b532619e3c98dc1e4450ec10a
Diego Toro <dtoro.a.s@gmail.com>
2023-08-31 01:11:40 -0400

    update home page

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
           component={motion.div}
           animate={controls}
           custom={0}
-          color="secondary"
+          color="primary"
           variant="h5"
           sx={{ marginBottom: "0px" }}
         >
@@ -76,7 +76,7 @@ const Home = () => {
           animate={controls}
           custom={3}
           variant="body2"
-          color="secondary"
+          color="primary"
           sx={{ marginBottom: "0" }}
         >
           {t("home_job")}
@@ -86,7 +86,7 @@ const Home = () => {
           animate={controls}
           custom={4}
           variant="body1"
-          color="secondary"
+          color="primary"
           sx={{ marginBottom: "30px" }}
         >
           {t("home_location")}
@@ -100,7 +100,7 @@ const Home = () => {
             duration={500}
             to="contact"
             variant="outlined"
-            color="secondary"
+            color="primary"
             size="large"
           >
             {t("home_contact_btn")}
